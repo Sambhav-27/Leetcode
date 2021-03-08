df431e6b2129dfb055f696d05d326a642f171372
Sambhav-27 <iamsambhavyadav@gmail.com>
2021-03-08 11:05:25 +0530

    Create Word_Break_II_140.cpp

---
+++ Word_Break_II_140.cpp
@@ -0,0 +1,46 @@
+// backtracking
+// do it carefully, seems complicated.
+// time: n^n; At each step, tree can have n options in worst case
+
+class Solution {
+public:
+    
+    string s;
+    unordered_set<string> st;
+    vector<string> ans;
+    
+    void fun(int j, vector<string> curSent) {
+        if(j == s.length()) {
+            string sent;
+            for(auto x: curSent) { // form sentence string from words
+                sent += x; sent+= " ";
+            }
+            sent.pop_back(); // remove last space
+            ans.push_back(sent);
+            return;
+        }
+        
+        string cur;
+        for(int i=j; i<s.length(); ++i) {
+            cur += s[i];
+            if(st.find(cur) != st.end()) {
+                curSent.push_back(cur);
+                fun(i+1, curSent);
+                curSent.pop_back();
+            }
+        }
+
+    }
+    
+    
+    vector<string> wordBreak(string str, vector<string>& wordDict) {
+        s = str;
+        for(auto x: wordDict) {
+            st.insert(x);
+        }
+        
+        vector<string> v(0);
+        fun(0, v);
+        return ans;
+    }
+};
