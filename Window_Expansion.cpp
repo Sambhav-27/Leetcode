796c2eefb76bdfc36b18009f19d128443cad5238
Sambhav-27 <iamsambhavyadav@gmail.com>
2021-04-06 23:50:53 +0530

    Create Window_Expansion.cpp

---
+++ Window_Expansion.cpp
@@ -0,0 +1,39 @@
+// learn this window exapnsion technique
+// also similar: https://leetcode.com/problems/minimum-window-substring/
+
+// Current problem: https://www.lintcode.com/problem/longest-substring-with-at-most-k-distinct-characters/
+
+#define N 256
+class Solution {
+public:
+    
+    int getChars(int * f) {
+        int c =0;
+        for(int i=0; i<N; ++i) {
+            if(f[i] > 0)
+            c++;
+        }
+        return c;
+    }
+   
+    int lengthOfLongestSubstringKDistinct(string &s, int k) {
+        int l=0, r=0;
+        int f[N] = {0};
+        int ans = 0;
+
+        f[s[0]]++; // notice this
+        while(r < s.length()) {
+            // squeeze from left
+            while(l <=r && getChars(f) > k) {
+                f[s[l++]]--;
+            }
+
+            ans = max(ans, r-l+1);
+            ++r;
+            if(r <s.length())
+                f[s[r]]++;
+            
+        }
+        return ans;
+    }
+};
