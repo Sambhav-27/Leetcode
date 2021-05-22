49b11e05212d759df3f32e5778ff83383ad83a8f
Sambhav-27 <iamsambhavyadav@gmail.com>
2021-05-22 20:20:57 +0530

    Create Valid_palindrome_iii.cpp

---
+++ Valid_palindrome_iii.cpp
@@ -0,0 +1,9 @@
+Given a string s and an integer k, find out if the given string is a K-Palindrome or not.
+
+A string is K-Palindrome if it can be transformed into a palindrome by removing at most k characters from it.
+
+
+let m = longest palindromic subsequence.
+
+then ans is true if
+s.length() - m <= k
