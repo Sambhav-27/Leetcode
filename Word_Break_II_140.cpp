1030f712d24aa47826f717f11b8cfe1bbc9ef098
Sambhav-27 <iamsambhavyadav@gmail.com>
2021-05-30 21:01:13 +0530

    Update Word_Break_II_140.cpp

--- Word_Break_II_140.cpp
+++ Word_Break_II_140.cpp
@@ -2,6 +2,8 @@
 // do it carefully, seems complicated.
 // time: n^n; At each step, tree can have n options in worst case
 // we can even store the total sentences for every index but these words can be too many; So better to avoid it.
+// time is I think 2^n * n; n from searching in dict. 2^n from recursion. After every point we have 2 options whethet to add a break here or not.
+// Also, worst case would happen when dict has all the substrings of the string. Total substring = 2^n
 
 class Solution {
 public:
@@ -24,7 +26,7 @@ public:
         string cur;
         for(int i=j; i<s.length(); ++i) {
             cur += s[i];
-            if(st.find(cur) != st.end()) {
+            if(st.find(cur) != st.end()) { // O(n) from it
                 curSent.push_back(cur);
                 fun(i+1, curSent);
                 curSent.pop_back();
