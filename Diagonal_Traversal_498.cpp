c4e0aed8de41d902f7cabb84ef48e7472ed2f970
Sambhav-27 <iamsambhavyadav@gmail.com>
2021-06-01 21:40:09 +0530

    Update Diagonal_Traversal_498.cpp

--- Diagonal_Traversal_498.cpp
+++ Diagonal_Traversal_498.cpp
@@ -79,3 +79,40 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+// for every diagonal (i+j) is constant
+// if you want diagnoals on other side then j-i is same, I think; Then we would have to use a map cause j-i can be negative
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& a) {
+        
+        
+        int n = a.size();
+        int m = a[0].size();
+                
+        vector<vector<int>> v(n+m); // total n+m diagonals
+        
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<m; ++j) {
+                v[i+j].push_back(a[i][j]);
+            }
+        }
+        
+        vector<int> ans;
+        
+        for(int i=0; i<v.size(); ++i) {
+            if(i%2 == 0) {
+                ans.insert(ans.end(), v[i].rbegin(), v[i].rend()); // traverse vector in reverse using rbegin & rend
+            }
+            else {
+                ans.insert(ans.end(), v[i].begin(), v[i].end());
+            }
+        }
+        return ans;
+    }
+};
