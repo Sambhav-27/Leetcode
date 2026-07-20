9c1c4f14a196ed5408b79c507a6a2c4caa6a6c34
Sambhav-27 <iamsambhavyadav@gmail.com>
2026-07-19 22:27:29 -0700

    Update slidingWindowMax_usingDeque.cpp

--- slidingWindowMax_usingDeque.cpp
+++ slidingWindowMax_usingDeque.cpp
@@ -1,3 +1,62 @@
+/**
+ 
+ 
+ - deque will stores indices of elements cuz we would need to remove out of range elements from queu
+ - remove out of range elements from front of dq.
+ - dq will be sorted with index of largest element at front, and smallest at back.
+ - new element will be added from rear;
+ - before adding, remove all smaller elements from back
+ 
+ - max element will be at front of dq.
+ 
+ 
+ */
+ 
+
+class Solution {
+public:
+
+    deque<int> q;
+
+    void insert(vector<int> &a, int index) {
+        // pop while element at end is smaller than new element
+        // to maintain monotonic increasing order
+        while(!q.empty() && a[q.back()] < a[index]) {
+            q.pop_back();
+        }
+        q.push_back(index);
+    }
+
+    vector<int> maxSlidingWindow(vector<int>& a, int k) {
+
+        
+        vector<int> ans;
+        for(int i=0; i<k; ++i) {
+            insert(a, i);
+        }
+        ans.push_back(a[q.front()]);
+
+        for(int i=k; i<a.size(); ++i) { // notice that i tracks end of window
+
+            // pop out of index element from front; 
+            // because front has the oldest elements as we're inserting from the back
+            while(!q.empty() && q.front() <= i-k) {
+                q.pop_front();
+            }
+
+            insert(a, i);
+            ans.push_back(a[q.front()]);
+        }
+
+        return ans;
+    }
+};
+
+
+// ----------------------
+	
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define N 10000007
