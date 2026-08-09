0cceda46cf2d534eba5ad0dff04d2f789afb4487
Sambhav-27 <iamsambhavyadav@gmail.com>
2026-08-08 19:51:30 -0700

    Update CloneGraph_133.cpp

--- CloneGraph_133.cpp
+++ CloneGraph_133.cpp
@@ -4,6 +4,100 @@ class Node {
 public:
     int val;
     vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+/*
+
+
+Just do a dfs a create a copy of all the nodes.
+
+Just keep track of all the nodes that you have created before; Do not recreate a node;
+Add the children irrespective of whether they have been visited before or not because we need to add all the edges.
+
+
+*/
+
+
+class Solution {
+    
+public:
+
+     vector<bool> visited = vector<bool>(104, 0); // if declared inside a class as only "vector<bool> visited(104, 0)" then it gets treated as a function declaration and it doesn't work 
+    unordered_map<int, Node*> m;
+
+
+    void dfs(Node* node, Node* cloneRoot) {
+        visited[node->val] = 1;
+
+        for(auto child: node->neighbors) {
+            Node* cloneChild; 
+            
+            if(m.find(child->val) != m.end()) { // node already exists
+                cloneChild = m[child->val];
+            }
+            else {
+                cloneChild = new Node(child->val);    // don't create new Node with original's neighbors; let it fill using dfs.
+                m[child->val] = cloneChild; // don't forget to add to map
+            }
+            
+            cloneRoot->neighbors.push_back(cloneChild);
+
+
+            if(!visited[child->val]) {
+
+                dfs(child, cloneChild);
+            }
+            
+        }
+
+    }
+
+    Node* cloneGraph(Node* node) {
+
+        if(!node) return NULL;
+
+        Node* cloneRoot = new Node(node->val);
+        m[node->val] = cloneRoot; // don't forget to add root to map
+
+        dfs(node, cloneRoot);
+
+        return cloneRoot;
+        
+    }
+};
+
+
+-----------------------------
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
     
     Node() {
         val = 0;
