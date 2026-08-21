4b4d113710588c1a85a4661fd95248564c980b86
Sambhav-27 <iamsambhavyadav@gmail.com>
2026-08-20 20:11:16 -0700

    Update Subsets_ii_90.cpp

--- Subsets_ii_90.cpp
+++ Subsets_ii_90.cpp
@@ -50,3 +50,57 @@ public:
         
     }
 };
+
+
+----------
+
+
+    class Solution {
+public:
+    
+    // if ith bit is set in num then include ith number from array in this subset.
+    // with this logic, number of elements in array has to be < 32
+    vector<int> generateOneSubset(vector<int> &a, int n) {
+        vector<int> subset;
+        for(int i=0; i<31; ++i) {
+            if(n & (1<<i)) { // this just checks if ith bit is set in n; if set then include this number in output
+                subset.push_back(a[i]); // this doesn't result in index our of bound because n is bounded by 2^a.size()
+            }
+        }
+        return subset;
+    }
+    
+    vector<vector<int>> ans;
+    vector<int> input;
+    
+    void fun(int j, vector<int> cur) {
+        if(j == input.size()) {
+            ans.push_back(cur); // only add to answer when it reaches end; because you add to answer when for all
+                            // elements you have made a decision to whether choose it or not choose it (i.e. only at leaf node of tree)
+            return;
+        }
+
+        // choose this
+        cur.push_back(input[j]);
+        fun(j+1, cur);
+        cur.pop_back();
+        
+        // or don't choose this
+        fun(j+1, cur);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& a) {
+
+//         int n = pow(2, a.size());
+//         vector<vector<int>> ans;
+//         for(int i=0; i<n; ++i) { // notice n = 2^a.size()
+//             ans.push_back(generateOneSubset(a, i));
+//         }
+//         return ans;
+        
+        input = a;
+        vector<int> v;
+        fun(0, v);
+        return ans;
+    }
+};
